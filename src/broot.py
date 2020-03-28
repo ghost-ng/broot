1d2014c73440fef8f390c1982f2fae7baefbf64f
midnightseer <21272239+MidnightSeer@users.noreply.github.com>
2020-03-27 22:44:48 -0400

    f

--- src/broot.py
+++ src/broot.py
@@ -10,16 +10,6 @@ import art
 import save
 import validate
 
-import colorama
-colorama.init()
- 
-def move_cursor(x,y):
-    print ("\x1b[{};{}H".format(y+1,x+1))
- 
-def clear():
-    print ("\x1b[2J")
-
-
 plugin = "/broot"
 prompt = plugin + "/>> "
 end_prgm_flag = False
@@ -97,10 +87,8 @@ def parse_cmds(cmds):
                 if validate.validate_options() is True:
                     print("Starting in [3]", end="\r", flush=True)
                     sleep(1.6)
-                    move_cursor(0,0)
                     print("Starting in [2]", end="\r", flush=True)
                     sleep(1.6)
-                    move_cursor(0,0)
                     print("Starting in [1]")
                     sleep(1.5)
                     print_info("Broot'ing...")
